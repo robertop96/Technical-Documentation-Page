49836900e9dcd798ff21084f858d3db666e522b3
robertop96 <roberto.manzano96@gmail.com>
2020-08-20 11:25:25 -0400

    fixed heading in 320px viewport

--- main.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementsByClassName("nav-link");
-    if (x.style.display === "none") {
-      x.style.display = "flex";
-    } else {
-      x.style.display = "none";
-    }
-  }
